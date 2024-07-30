ae8d4d4823d7c54e1fb3cad540fd19d9fe4b7868
MaxiFabbri <63376220+MaxiFabbri@users.noreply.github.com>
2024-07-30 16:25:46 -0300

    Update carga-datos-por-csv.sql

--- sql_project/carga-datos-por-csv.sql
+++ sql_project/carga-datos-por-csv.sql
@@ -4,7 +4,7 @@
 SET GLOBAL local_infile = true;
 
 
-LOAD DATA LOCAL INFILE './data/Proveedores_Quattrum_Modif.csv' 
+LOAD DATA LOCAL INFILE './data_csv/Proveedores_Quattrum_Modif.csv' 
 INTO TABLE merchanmanager.proveedor 
 FIELDS TERMINATED BY ';' 
 ENCLOSED BY '"' 
@@ -20,7 +20,7 @@ IGNORE 1 ROWS
 ,   activo
 );
 
-LOAD DATA LOCAL INFILE './data/Clientes_Quattrum_Modif.csv' 
+LOAD DATA LOCAL INFILE './data_csv/Clientes_Quattrum_Modif.csv' 
 INTO TABLE merchanmanager.cliente 
 FIELDS TERMINATED BY ';' 
 ENCLOSED BY '"' 
@@ -36,7 +36,7 @@ IGNORE 1 ROWS
 ,   activo
 );
 
-LOAD DATA LOCAL INFILE './data/pedidos.csv' 
+LOAD DATA LOCAL INFILE './data_csv/pedidos.csv' 
 INTO TABLE merchanmanager.pedido 
 FIELDS TERMINATED BY ';' 
 ENCLOSED BY '"' 
@@ -54,7 +54,7 @@ IGNORE 1 ROWS
 ,   observaciones
 );
 
-LOAD DATA LOCAL INFILE './data/item.csv' 
+LOAD DATA LOCAL INFILE './data_csv/item.csv' 
 INTO TABLE merchanmanager.item 
 FIELDS TERMINATED BY ';' 
 ENCLOSED BY '"' 
@@ -68,7 +68,7 @@ IGNORE 1 ROWS
 ,   costo_unitario
 );
 
-LOAD DATA LOCAL INFILE './data/remito.csv' 
+LOAD DATA LOCAL INFILE './data_csv/remito.csv' 
 INTO TABLE merchanmanager.remito 
 FIELDS TERMINATED BY ';' 
 ENCLOSED BY '"' 
@@ -82,7 +82,7 @@ IGNORE 1 ROWS
 ,   detalle
 );
 
-LOAD DATA LOCAL INFILE './data/item_remito.csv' 
+LOAD DATA LOCAL INFILE './data_csv/item_remito.csv' 
 INTO TABLE merchanmanager.item_remito 
 FIELDS TERMINATED BY ';' 
 ENCLOSED BY '"' 
@@ -93,7 +93,7 @@ IGNORE 1 ROWS
 ,	producto
 );
 
-LOAD DATA LOCAL INFILE './data/movimiento_pedido.csv' 
+LOAD DATA LOCAL INFILE './data_csv/movimiento_pedido.csv' 
 INTO TABLE merchanmanager.movimiento_pedido
 FIELDS TERMINATED BY ';' 
 ENCLOSED BY '"' 
@@ -105,7 +105,7 @@ IGNORE 1 ROWS
     id_estado_pedido
 );
 
-LOAD DATA LOCAL INFILE './data/movimiento_item.csv' 
+LOAD DATA LOCAL INFILE './data_csv/movimiento_item.csv' 
 INTO TABLE merchanmanager.movimiento_item
 FIELDS TERMINATED BY ';' 
 ENCLOSED BY '"' 
